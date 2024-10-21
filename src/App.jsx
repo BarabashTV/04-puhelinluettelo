c99f61e857ecdae126e2577548ee13cca29d5880
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2024-10-21 12:29:28 +0300

    Alkutila haetaan Axios-kirjaston avulla palvelimelta. Hoida datan hakeminen Effect hookilla.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456" },
-    { name: "Ada Lovelace", phone: "39-44-5323523" },
-    { name: "Dan Abramov", phone: "12-43-234345" },
-    { name: "Mary Poppendieck", phone: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [phone, setPhone] = useState("");
   const [check, setCheck] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const handleNewNameChange = (event) => {
     console.log("new name click", event.target.value);
@@ -40,7 +45,7 @@ const App = () => {
 
     const newPerson = {
       name: newName,
-      phone: phone,
+      number: phone,
     };
     setPersons(persons.concat(newPerson));
     setNewName("");
